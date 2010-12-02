8368b01b7ecf114c2a9c3e72c9384cb5c8be3a03
Igor Czerwinski <igoratron@users.noreply.github.com>
2010-12-02 06:54:22 +0000

    new Pieces and Treemap

--- jigsaw/jigsaw/MainWindow.xaml.cs
+++ jigsaw/jigsaw/MainWindow.xaml.cs
@@ -11,6 +11,8 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using System.Xml;
+using DavidWynne.TreeMap.Silverlight.Model;
 
 namespace jigsaw
 {
@@ -19,9 +21,66 @@ namespace jigsaw
     /// </summary>
     public partial class MainWindow : Window
     {
+        private static Random random = new Random();
         public MainWindow()
         {
             InitializeComponent();
+
+            generateTreeMap(loadDbDescription());
+        }
+
+        public List<InputValue> loadDbDescription()
+        {
+            List<InputValue> result = new List<InputValue>();
+            XmlTextReader reader = new XmlTextReader("../../database.xml");
+
+            while (reader.Read())
+            {
+                if (XmlNodeType.Element.Equals(reader.NodeType) &&
+                    "table".Equals(reader.Name))
+                {
+                    String name = null;
+                    Int32 size = 0;
+                    while (reader.MoveToNextAttribute())
+                    {
+                        switch (reader.Name)
+                        {
+                            case "size":
+                                size = Int32.Parse(reader.Value);
+                                break;
+                            case "name":
+                                name = reader.Value;
+                                break;
+                        }
+                    }
+                    result.Add(new InputValue(name, size));
+                }
+            }
+
+            return result;
+        }
+
+        public void generateTreeMap(List<InputValue> values)
+        {
+            List<Sector> sectors = TreeMap.Plot(values, new Size(400, 300));
+
+            foreach (Sector sector in sectors)
+            {
+                Piece p = new Piece();
+                p.Rect = sector.Rect;
+                p.Direction = "South";
+                p.Color = getRandomBrush();
+                canvas.Children.Add(p);
+            }
+        }
+
+        private Brush getRandomBrush()
+        {
+            Brush[] brushes = {
+                                  Brushes.Red, Brushes.Orange, Brushes.Yellow, Brushes.Lime, Brushes.Green, Brushes.Purple, Brushes.Blue
+                              };
+
+            return brushes[random.Next(brushes.Length)];
         }
     }
 }
---
+++ jigsaw/jigsaw/TypeConverters/SizeConverter.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Data;
+using System.Windows;
+
+namespace jigsaw
+{
+    class RectConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is Size)
+            {
+                return new Rect((Size)value);
+            }
+            else
+            {
+                return null;
+            }
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
