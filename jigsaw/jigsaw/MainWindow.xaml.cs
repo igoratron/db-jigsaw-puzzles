adfcaf8243a0ebe33b30079d9ec65af26094bc3b
Igor Czerwinski <igoratron@users.noreply.github.com>
2010-12-28 21:09:08 +0000

    working tabs

--- jigsaw/jigsaw/MainWindow.xaml.cs
+++ jigsaw/jigsaw/MainWindow.xaml.cs
@@ -12,7 +12,6 @@ using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
 using System.Xml;
-using DavidWynne.TreeMap.Silverlight.Model;
 
 namespace jigsaw
 {
@@ -25,69 +24,6 @@ namespace jigsaw
         public MainWindow()
         {
             InitializeComponent();
-
-            generateTreeMap(loadDbDescription());
-        }
-
-        public List<InputValue> loadDbDescription()
-        {
-            List<InputValue> result = new List<InputValue>();
-            XmlTextReader reader = new XmlTextReader("../../database.xml");
-
-            while (reader.Read())
-            {
-                if (XmlNodeType.Element.Equals(reader.NodeType) &&
-                    "table".Equals(reader.Name))
-                {
-                    string name = null;
-                    int size = 0;
-                    string relationTo = null;
-                    while (reader.MoveToNextAttribute())
-                    {
-                        switch (reader.Name)
-                        {
-                            case "size":
-                                size = int.Parse(reader.Value);
-                                break;
-                            case "name":
-                                name = reader.Value;
-                                break;
-                            case "foreignKey":
-                                relationTo = reader.Value;
-                                break;
-                        }
-                    }
-                    result.Add(new InputValue(name, size, relationTo));
-                    dbView.Items.Add(new InputValue(name, size, relationTo));
-               } 
-            }
-
-            return result;
-        }
-
-        public void generateTreeMap(List<InputValue> values)
-        {
-            List<Sector> sectors = TreeMap.Plot(values, new Size(400, 300));
-
-            for(int i = 0; i < sectors.Count; i++)
-            {
-                Piece p = new Piece();
-                Sector current = sectors[i];
-                p.Rect = current.Rect;
-                p.Direction = current.RelationTo;
-                p.TableName = current.Name;
-                p.Color = getBrush(i);
-                canvas.Children.Add(p);
-            }
-        }
-
-        private Brush getBrush(Int32 i)
-        {
-            Brush[] brushes = {
-                                  Brushes.Red, Brushes.Lime, Brushes.Purple, Brushes.Orange, Brushes.Yellow, Brushes.Blue, Brushes.Green, 
-                              };
-
-            return brushes[i % brushes.Length];
         }
     }
 }
