2fa7a496f3c9bf780c89a0a1f356e2306d34ab6f
Igor Czerwinski <igoratron@users.noreply.github.com>
2011-01-04 21:37:32 +0000

    model + jigsaw board, does not work very well

---
+++ jigsaw/jigsaw/Jigsaw/JigsawBoard.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows.Controls;
+using System.Windows;
+using System.Xml;
+using jigsaw.Model;
+
+namespace jigsaw.Jigsaw
+{
+    class JigsawBoard : Panel
+    {
+        private double totalArea = 0;
+        private double ratio = 0;
+
+        protected override Size MeasureOverride(Size availableSize)
+        {
+            double totalWidth = 0;
+            double totalHeight = 0;
+
+            //calculate the sum
+            foreach (UIElement child in Children)
+            {
+                if ((child as ContentPresenter).Content is Table)
+                {
+                    Table t = (child as ContentPresenter).Content as Table;
+                    totalArea += t.Size;
+                }
+            }
+
+            ratio = availableSize.Height * availableSize.Width / totalArea;
+
+            foreach (UIElement child in Children)
+            {
+                Table t = (child as ContentPresenter).Content as Table;
+                double width = Math.Sqrt(t.Size * ratio);
+                child.Measure(new Size(width, width));
+                
+                Size childSize = child.DesiredSize;
+                totalWidth += childSize.Width;
+                totalHeight += childSize.Height;
+            }
+
+            return new Size(totalWidth, totalHeight);
+        }
+
+         protected override Size ArrangeOverride( Size finalSize ) {
+            Point currentPosition = new Point( );
+
+            foreach( UIElement child in Children ) {
+                Rect childRect = new Rect( currentPosition, child.DesiredSize );
+                child.Arrange( childRect );
+                currentPosition.Offset( childRect.Width, childRect.Height );
+            }
+
+            return new Size( currentPosition.X, currentPosition.Y );
+        }
+    }
+}
---
+++ jigsaw/jigsaw/Model/SchemaViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Collections.ObjectModel;
+using System.Windows.Data;
+using System.Xml;
+
+namespace jigsaw.Model
+{
+    class SchemaViewModel
+    {
+        public ObservableCollection<Table> Schema { get; set; }
+        public String XmlSchemaPath
+        {
+            set
+            {
+                XmlTextReader reader = new XmlTextReader("../../" + value);
+                reader.Read();
+                while (reader.Read())
+                {
+                    if(XmlNodeType.Element.Equals(reader.NodeType) && "table".Equals(reader.Name))
+                    {
+                        Schema.Add(new Table(reader.GetAttribute("name"), reader.GetAttribute("size")));
+                    }
+                }
+            }
+        }
+
+        public SchemaViewModel()
+        {
+            Schema = new ObservableCollection<Table>();
+        }
+
+
+    }
+}
