062cc3cbe9c558558c85ccbc2b74d69fd023da8f
czerwony <czerwony@1b5723fd-7c4e-0158-e04a-91ca3fab1ad7>
2011-02-06 08:53:04 +0000

    tabs working again

--- trunk/jigsaw/jigsaw/TypeConverters/TabCenterConverter.cs
+++ trunk/jigsaw/jigsaw/TypeConverters/TabCenterConverter.cs
@@ -20,7 +20,10 @@ namespace jigsaw.TypeConverters
                 Point centre = new Point();
 
                 double angle = Math.Atan2(otherPiece.Y + otherPiece.Height / 2 - thisPiece.Y - thisPiece.Height / 2, otherPiece.X + otherPiece.Width / 2 - thisPiece.X - thisPiece.Width / 2);
-                System.Diagnostics.Debug.Assert(!double.IsNaN(angle), "Angle is NAN");
+
+                if (double.IsNaN(angle))
+                    return null;
+
                 double r1sqrd = thisPiece.Width * thisPiece.Width / (4 * Math.Cos(angle) * Math.Cos(angle));
                 double r2sqrd = thisPiece.Height * thisPiece.Height / (4 * Math.Sin(angle) * Math.Sin(angle));
 
